9cfa599ef278591ee6410c1554cf5db8d95f3bea
srijitcoder <srijitsm123@gmail.com>
2026-03-18 20:24:47 +0530

    fix: preview url

--- osc/schema-map/create-schema-map.js
+++ osc/schema-map/create-schema-map.js
@@ -1,6 +1,6 @@
 export default function createSchemaMap() {
   const preview = new URL(import.meta.url).href.replace(
-    "/osc-config.mjs",
+    "/create-schema-map.js",
     "/osc.html",
   );
   const schemaDefaults = {
